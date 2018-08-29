9bb768db81a64d6eed287c0e6b8d1110122f44b2
Aiden Garipoli <aidosgaripoli@gmail.com>
2018-08-29 20:43:52 +1000

    initial commit

---
+++ src/index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express()
+const http = require('http').Server(app)
+const io = require('socket.io')(http, { transports: ['websocket', 'polling'] })
+const redisAdapter = require('socket.io-redis')
+
+io.adapter(redisAdapter({ host: 'localhost', port: 6379 }))
+
+app.get('/', (req, res) => {
+  res.status(200).sendFile(`${__dirname}/index.html`)
+})
+
+io.on('connection', socket => {
+  console.log('User connected.')
+
+  socket.on('message', message => {
+    console.log(`Recevved: ${message}`)
+    socket.broadcast.emit('message', message)
+  })
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected.')
+  })
+})
+
+http.listen(3000, console.log('Listening on port 3000'))
